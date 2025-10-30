8e3661e508e57ee6d5080d42d4805f42457e8f2d
iagooteles <iagooteles@gmail.com>
2025-10-30 20:41:15 -0300

    iniciado desenvolvimento entidade games

---
+++ src/main/java/com/los_pitufs/los_pitufs/model/Game.java
@@ -0,0 +1,45 @@
+package com.los_pitufs.los_pitufs.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "games")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    private String description;
+    private String developer;
+    private String publisher;
+    private LocalDate releaseDate;
+    private String coverImageUrl;
+    private String genre;
+
+    // Um jogo pode ter várias avaliações (de usuários diferentes)
+    @OneToMany(mappedBy = "game", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews;
+
+    // Construtores, getters e setters
+    public Game() {}
+
+    public Game(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+    
+}
